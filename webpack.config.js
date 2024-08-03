45623ba07062ba41fcdfe3c28c0e121031aaa5e6
Santosh <santoshssk923@gmail.com>
2024-08-03 11:32:19 +0530

    babel rc changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Your entry file
+  entry: './index.js', // Your entry file
   output: {
     filename: 'bundle.js', // Output bundle file
     path: path.resolve(__dirname, 'dist'), // Output directory
